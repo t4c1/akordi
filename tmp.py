78f17c0cd88758cfe4160583817971b58519c2ca
Tadej <tadej.c@gmail.com>
2015-04-08 21:23:00 +0200

    pyguitarpro demo

---
+++ tmp.py
@@ -0,0 +1,19 @@
+__author__ = 'Tadej'
+import guitarpro
+print "start"
+a=guitarpro.parse(r"Guns N Roses - Patience.gp5")
+for track in a.tracks[0:1]:
+    print track,track.name
+    for measure in track.measures:
+        print "\t",measure
+        for voice in measure.voices:
+            #print "\t\t",voice
+            for beat in voice.beats:
+                d=beat.duration
+                print "\t\t",beat.start,beat.index,beat.octave,d.value
+                for note in beat.notes:
+                    print "\t\t\t",note.value,note.velocity,note.string,note.durationPercent
+print note.beat
+
+#start cetrtinka=960
+#1/duration je trajanje v celinkah
